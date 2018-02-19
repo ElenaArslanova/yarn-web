468ca8d9aa2c836216c3b39a33a0892bb3476d6e
MaxMayer <endurancemayer@yandex.ru>
2018-02-20 00:00:07 +0500

    js объект для ajax post на клиенте, управление стилями

---
+++ static/js/handlingchanges.js
@@ -0,0 +1,18 @@
+
+function BagOfElements() {
+	
+	this.getSynsetResults = function(parentElement) {
+		var children = parentElement.getElementsByTagName('li');
+		var correct = [];
+		var wrong = [];
+		for (var i=0; i < children.length; i++) {
+			if (children[i].classList[1] === 'green-box')
+				correct.push(children[i].textContent);
+			else
+				wrong.push(children[i].textContent);
+		}
+		return {'correct' : correct, 'wrong' : wrong, 'id' : parentElement.id};
+	}
+}
+
+var bag = new BagOfElements();
